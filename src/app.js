10816c41665b9cfc80b8e14976e1353667b275ae
Cld338 <jihuno291@gmail.com>
2026-03-08 18:10:13 +0900

    sitemap 추가

--- src/app.js
+++ src/app.js
@@ -47,7 +47,31 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../public', 'index.html'));
 });
 
+// sitemap.xml 라우터 추가
+app.get('/sitemap.xml', (req, res) => {
+    const host = req.get('host');
+    const protocol = req.protocol;
+    const baseUrl = `${protocol}://${host}`;
 
+    // 허용된 페이지 목록 (우선 메인 페이지 포함)
+    const allowedPages = [
+        { url: '/', changefreq: 'daily', priority: '1.0' },
+        // 추가 페이지 예시: { url: '/docs', changefreq: 'weekly', priority: '0.8' }
+    ];
+
+    const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+        <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+        ${allowedPages.map(page => `
+        <url>
+            <loc>${baseUrl}${page.url}</loc>
+            <lastmod>2026-03-08</lastmod>
+            <changefreq>${page.changefreq}</changefreq>
+            <priority>${page.priority}</priority>
+        </url>`).join('')}
+        </urlset>`;
+    res.header('Content-Type', 'application/xml');
+    res.send(sitemap);
+});
 
 // 파일 정리 스케줄러 실행
 startCleanupJob();
